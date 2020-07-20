e8b3400e0ab0b6a5a654544e6bc3f49e44d53929
kimberlin99 <alisa.uth99@gmail.com>
2020-07-20 21:34:56 +0700

    Delete unnecessary code

--- server/index.js
+++ server/index.js
@@ -49,14 +49,6 @@ app.use('/products', productRoute)
 app.use('/products/animals', productAnimalRoute)
 app.use('/user', userRoute)
 
-// Serve static files from the React frontend app
-app.use(express.static(path.join(__dirname, '../client/build')))
-
-// AFTER defining routes: Anything that doesn't match what's above, send back index.html; (the beginning slash ('/') in the string is important!)
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname + '/../client/build/index.html'))
-})
-
 app.listen(process.env.PORT || 8080, () => {
   console.log(`Server starts at port ${process.env.PORT || 8080}`)
 })
